084a165380a1b34f5614fc30aa6012f919a4dd0d
Rostom <88782107+Rostom12@users.noreply.github.com>
2022-04-12 14:36:12 +0200

    Add files via upload

---
+++ CélébritésPHP/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="test">
+
+    <title><?php echo PROJECT_NAME; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="bootstrap/js/jquery-1.9.0.min.js"></script>
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-static-top" role="navigation">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/address_book_beau/" target="_blank"><span class="glyphicon glyphicon-home"></span> Carnet d'adresses</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mainbody">
+      <div class="page-header">
+        <h1>Carnet d'adresses</h1>
+      </div>
+      <div class="clearfix"></div>
